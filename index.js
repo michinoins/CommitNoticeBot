f7d07f0375ddca17a434a23a3a51787b0c13d1b2
michinoins <m1i1k2i1ya@gmail.com>
2021-11-24 23:08:53 +0900

    add: json parse

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ query($userName:String!) {
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
-      'Authorization': `Bearer `,
+      'Authorization': `Bearer  `,
     },
   })
 
 // アカウント情報取得
 async function getAccountData(){
   const response = await client.post('graphql', { query,variables:{"userName":"michinoins"}})
-  console.log(JSON.stringify(response.data))
+  console.log(JSON.stringify(response.data["data"]["user"]["contributionsCollection"]["contributionCalendar"]["weeks"].slice(-1)[0]["contributionDays"].slice(-1)[0]["contributionCount"]))
 }
 
 getAccountData()
