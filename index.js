f9492042da804ae518b31d4d97c958a2ed82a880
michinoins <m1i1k2i1ya@gmail.com>
2021-11-26 09:35:33 +0900

    add: linebotSdk , get Commit Count by gql

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const axios = require("axios")
+const line = require('@line/bot-sdk')
 
-// スキーマを定義する
+exports.handler = async (event) => {
+     const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+const client = new line.Client({
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
+
+const commitCount = await getCommitMessage()
+
+const message = {
+  type:'text',
+  text:`今日のコミット数は${commitCount}でした`
+}
+await client.pushMessage(process.env.BOT_ID, message).then(() => {
+  }).catch((err) => {
+    // error handling
+  });
+}
+
+// 今日のコミット数を取得する
+async function getCommitMessage(){
+  
+// 取得クエリ
 const query = `
 query($userName:String!) { 
     user(login: $userName){
@@ -17,20 +43,21 @@ query($userName:String!) {
       }
     }
   }`
-
-  const client = axios.create({
+  // Graph QL クライアント
+  const graphClient = axios.create({
     baseURL: 'https://api.github.com/',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
-      'Authorization': `Bearer  `,
+      'Authorization': `Bearer ${process.env.GITHUB_GRAPH_QL_TOKEN}`,
     },
   })
-
-// アカウント情報取得
-async function getAccountData(){
-  const response = await client.post('graphql', { query,variables:{"userName":"michinoins"}})
-  console.log(JSON.stringify(response.data["data"]["user"]["contributionsCollection"]["contributionCalendar"]["weeks"].slice(-1)[0]["contributionDays"].slice(-1)[0]["contributionCount"]))
+  
+  // アカウント情報取得
+  const response = await graphClient.post('graphql', { query,variables:{"userName":process.env.GITHUB_USERNAME}})
+  const CurrentDateCommitCount = JSON.stringify(response.data["data"]["user"]["contributionsCollection"]["contributionCalendar"]["weeks"]
+  .slice(-1)[0]["contributionDays"].slice(-1)[0]["contributionCount"])
+  
+  return CurrentDateCommitCount
 }
 
-getAccountData()
